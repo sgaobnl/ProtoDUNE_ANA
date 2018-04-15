f999cb3ffac89fec4f9964aac76a268544ae2c71
sgaobnl <gao33.bnl@gmail.com>
2018-04-15 16:54:29 -0400

    update

--- main.sh
+++ main.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# APA_analysis.py --> # apano   rmsdate      fpgdate     asidate     rmsrunno fpgarunno asicrunno apafolder jumbo_flag
+python APA_analysis.py   5   "04_12_2018" "04_12_2018" "04_12_2018" run08rms run04fpg   run03asi   APA40      True
+
+# apa_plot_main.py--> # apano   rmsdate      fpgdate     asidate     rmsrunno fpgarunno asicrunno apafolder 
+python apa_plot_main.py  5   "04_12_2018" "04_12_2018" "04_12_2018" run08rms   run04fpg  run03asi  APA40 
+
+# avg_chns_fft.py --> # apano   rmsdate      fpgdate      asidate     rmsrunno fpgarunno asicrunno apafolder jumbo_flag PSD_EN  PSD
+python avg_chns_fft.py   5   "04_12_2018" "04_12_2018" "04_12_2018"  run08rms  run04fpg  run03asi     APA40     True   False    0
+
+
+
+
+
+
                      # apano,   rmsdate,     fpgdate,     asidate, rmsrunno, fpgarunno, asicrunno, apafolder, jumbo_flag
 #python APA_analysis.py  5     "04_12_2018" "04_12_2018" "04_12_2018" run08rms   run04fpg  run03asi  Coldbox  False
 

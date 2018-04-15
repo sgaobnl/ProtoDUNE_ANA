e7e4cb9f3d953ca20a8a5a9d8827e81a6a817d4e
sgaobnl <gao33.bnl@gmail.com>
2018-04-15 16:52:08 -0400

    update

---
+++ chn_main.cmd
@@ -0,0 +1,4 @@
+::#!/bin/bash
+::                       # apano,   rmsdate,     fpgdate,     asidate, rmsrunno, fpgarunno, asicrunno, apafolder wibno, fembno, chnno
+python chn_plot_main.py  9     "04_14_2018" "04_14_2018" "04_14_2018" run01rms   run01fpg  run01asi  APA40       0       0      0
+

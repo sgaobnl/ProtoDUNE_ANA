1a4ca0bf5a1ef4623497d3955a4056cb13dc6376
sgaobnl <gao33.bnl@gmail.com>
2018-04-15 21:31:03 -0400

    update

--- main.cmd
+++ main.cmd
@@ -2,8 +2,8 @@
 ::python APA_analysis.py   9   "04_15_2018" "04_15_2018" "04_15_2018" run01rms run01fpg   run01asi   APA40      True
 ::
 :: apa_plot_main.py--> # apano   rmsdate      fpgdate     asidate     rmsrunno fpgarunno asicrunno apafolder 
-python apa_plot_main.py 9     "04_15_2018" "04_15_2018" "04_15_2018" run01rms   run01fpg  run01asi  APA40 
+::python apa_plot_main.py 9     "04_15_2018" "04_15_2018" "04_15_2018" run01rms   run01fpg  run01asi  APA40 
 ::
-:: avg_chns_fft.py --> # apano   rmsdate      fpgdate      asidate     rmsrunno fpgarunno asicrunno apafolder jumbo_flag PSD_EN  PSD
-::python avg_chns_fft.py     9   "04_15_2018" "04_15_2018" "04_15_2018"  run01rms  run01fpg  run01asi     APA40     True   False    0
+:: avg_chns_fft.py --> # apano   rmsdate      fpgdate      asidate     rmsrunno fpgarunno asicrunno apafolder jumbo_flag PSD_EN  PSD  wire_type gains tps
+python avg_chns_fft.py     9   "04_15_2018" "04_15_2018" "04_15_2018"  run01rms  run01fpg  run01asi     APA40     True   False    0       V      250  20
 

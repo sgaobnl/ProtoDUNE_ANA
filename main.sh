01acf590ecf09078d94f944fffaf1f69c6f51736
Shanshan Gao <gao33.bnl@gmail.com>
2018-09-21 19:38:48 -0400

    calibraton check

--- main.sh
+++ main.sh
@@ -5,8 +5,22 @@
 ## apa_plot_main.py--> # apano   rmsdate      fpgdate     asidate     rmsrunno fpgarunno asicrunno apafolder 
 #python apa_plot_main.py  3   "06_05_2018" "06_05_2018" "06_05_2018" run01rms   run01fpg  run01asi  APA 
 #
+python APA_analysis.py   1   "09_17_2018" "09_17_2018" "09_17_2018" run01rms run01fpg   run01asi   APA    False 
+python APA_analysis.py   2   "09_17_2018" "09_17_2018" "09_17_2018" run01rms run01fpg   run01asi   APA    False 
+python APA_analysis.py   3   "09_17_2018" "09_17_2018" "09_17_2018" run01rms run01fpg   run01asi   APA    False 
+python APA_analysis.py   4   "09_17_2018" "09_17_2018" "09_17_2018" run01rms run01fpg   run01asi   APA    False 
+python APA_analysis.py   5   "09_17_2018" "09_17_2018" "09_17_2018" run01rms run01fpg   run01asi   APA    False 
+python APA_analysis.py   6   "09_17_2018" "09_17_2018" "09_17_2018" run01rms run01fpg   run01asi   APA    False 
+
+
+python APA_analysis.py   1   "09_17_2018" "09_17_2018" "09_17_2018" run02rms run02fpg   run01asi   APA    False 
+python APA_analysis.py   2   "09_17_2018" "09_17_2018" "09_17_2018" run02rms run02fpg   run01asi   APA    False 
 python APA_analysis.py   3   "09_17_2018" "09_17_2018" "09_17_2018" run02rms run02fpg   run01asi   APA    False 
-python apa_plot_main.py  3   "09_17_2018" "09_17_2018" "09_17_2018" run02rms run02fpg  run01asi  APA 
+python APA_analysis.py   4   "09_17_2018" "09_17_2018" "09_17_2018" run02rms run02fpg   run01asi   APA    False 
+python APA_analysis.py   5   "09_17_2018" "09_17_2018" "09_17_2018" run02rms run02fpg   run01asi   APA    False 
+python APA_analysis.py   6   "09_17_2018" "09_17_2018" "09_17_2018" run02rms run02fpg   run01asi   APA    False 
+
+#python apa_plot_main.py  3   "09_17_2018" "09_17_2018" "09_17_2018" run02rms run02fpg  run01asi  APA 
 #python APA_analysis.py   3   "09_17_2018" "09_17_2018" "09_17_2018" run01rms run01fpg   run01asi   APA    False 
 #python apa_plot_main.py  3   "09_17_2018" "09_17_2018" "09_17_2018" run01rms run01fpg  run01asi  APA 
 #python avg_chns_fft.py   3   "09_17_2018" "09_17_2018" "09_17_2018" run01rms  run01fpg  run01asi     APA     False     False    0     X       140  20

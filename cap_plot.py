9547b4d9c05c0d77c7a7a752e31f02de18c639f6
Shanshan Gao <gao33.bnl@gmail.com>
2018-10-29 10:00:01 -0400

    update

--- cap_plot.py
+++ cap_plot.py
@@ -5,7 +5,7 @@ Author: GSS
 Mail: gao.hillhill@gmail.com
 Description: 
 Created Time: 7/15/2016 11:47:39 AM
-Last modified: 10/23/2018 5:44:46 PM
+Last modified: Wed Oct 24 12:16:05 2018
 """
 #import matplotlib
 #matplotlib.use('Agg')
@@ -39,6 +39,7 @@ from apa_plot_out import plot1_chns_enc
 from apa_plot_out import plot4_chns_gain
 from apa_plot_out import dict_filter
 from apa_plot_out import find_a_chn
+import statsmodels.api as sm
 
 APAno = int(sys.argv[1])
 rmsdate = sys.argv[2]
@@ -69,7 +70,8 @@ else:
 #fembs_on_apa = range(1,21, 1) 
 fembs_on_apa = range(1,2, 1) 
 
-sum_path = rms_rootpath + "/" + "results/" + "APA%d_"%APAno + rmsrunno + "_" + fpgarunno + "_" + asicrunno +"/"
+#sum_path = rms_rootpath + "/" + "results/" + "APA%d_"%APAno + rmsrunno + "_" + fpgarunno + "_" + asicrunno +"/"
+sum_path = "/Users/shanshangao/Google_Drive_BNL/tmp/caps/"+ "APA%d_"%APAno + rmsrunno + "_" + fpgarunno + "_" + asicrunno +"/" 
 fn = "APA%d"%APAno + "_" + rmsrunno + "_" + fpgarunno + "_" + asicrunno
 orgdicts = load_sum (sum_path, fn + ".allsum")
 
@@ -80,31 +82,41 @@ for fembloc in fembs_on_apa:
         femb_cs.append(["apaloc", "B" + format(APAno, "1d") + format(fembloc, "02d")])
     else:
         femb_cs.append(["apaloc", "A" + format(APAno, "1d") + format(fembloc, "02d")])
-asic5 = [151, 154, 100, 105, 51 , 51 , 35 , 37 , 25 , 25 , 53 , 55 , 5  , 5  , 105, 105]
+#asic5 = [151, 154, 100, 105, 51 , 51 , 35 , 37 , 25 , 25 , 53 , 55 , 5  , 5  , 105, 105]
+asic5 = [150, 150, 100, 100, 47 , 47 , 33 , 33 , 22 , 22 , 50 , 50 , 0  , 0  , 100, 100]
 asic5 = asic5[::-1]
-asic6 = [ 4  , 4  , 4  , 5  , 155, 155, 5  , 5  , 31 , 30 , 4  , 5  , 5  , 5  , 5  , 5]
+#asic6 = [ 4  , 4  , 4  , 5  , 155, 155, 5  , 5  , 31 , 30 , 4  , 5  , 5  , 5  , 5  , 5]
+asic6 = [ 0  , 0  , 0  , 0  , 150, 150, 0  , 0  , 27 , 27 , 0  , 0  , 0  , 0  , 0  , 0]
 asic6 = asic6[::-1]
-asic7 = [ 154 , 106 , 150 , 103 , 55  , 37  , 55  , 40  , 30  , 105 , 30  , 56  , 5   , 105 , 5   , 107 ]
-asic8 = [ 6   , 5   , 6   , 6   , 5   , 157 , 157 , 7   , 5   , 32  , 35  , 7   , 6   , 5   , 6   , 5   ]
+#asic7 = [ 154 , 106 , 150 , 103 , 55  , 37  , 55  , 40  , 30  , 105 , 30  , 56  , 5   , 105 , 5   , 107 ]
+asic7 = [ 150 , 100 , 150 , 100 , 47  , 33  , 47  , 33  , 22  , 50 , 22  , 50  , 0   , 100 , 0   , 100 ]
+#asic8 = [ 6   , 5   , 6   , 6   , 5   , 157 , 157 , 7   , 5   , 32  , 35  , 7   , 6   , 5   , 6   , 5   ]
+asic8 = [ 0   , 0   , 0   , 0   , 0   , 150 , 150 , 0   , 0   , 27  , 27  , 0   , 0   , 0   , 0   , 0   ]
 
 col_chncaps = asic5 + asic6 
-col_chnnos= np.array(range(len(col_chncaps))) + 64
+#col_chncaps = col_chncaps[0:2] + col_chncaps[16:] 
+col_chnnos= np.array(range(len(col_chncaps))) + 64 
+#col_chnnos = [64,65] + range(80,96,1)
+
+ind_chncaps = asic7 + asic8 
+ind_chnnos= np.array(range(len(ind_chncaps))) + 96 
+col_chncaps = ind_chncaps 
+col_chnnos = ind_chnnos
+
 col_dicts = []
 for chn in range(len(col_chnnos)):
     ads = find_a_chn(orgdicts, wibno=0, fembno=0, fembchn=col_chnnos[chn]) 
     for ad in ads:
         ad["Cd"] = col_chncaps[chn]
         col_dicts.append(ad) 
-print len(col_dicts)
 
-tp_dicts = dict_filter(col_dicts, and_dnf =[ ["tp", "20"],  ], or_dnf = [["fpgadac_en", True], ["asicdac_en", True]] ) 
+tp_dicts = dict_filter(col_dicts, and_dnf =[ ["tp", "30"],  ], or_dnf = [["fpgadac_en", True], ["asicdac_en", True]] ) 
 
 gs = ["250","140","078","047"]
 gs_cds = []
 gs_encs = []
 for g in gs:
     g_dicts = dict_filter(tp_dicts, and_dnf =[ ["gain", g],  ], or_dnf = [["fpgadac_en", True], ["asicdac_en", True]] ) 
-    print len(g_dicts)
     cds = []
     encs = []
     for chndict in g_dicts:
@@ -114,10 +126,69 @@ for g in gs:
     gs_cds.append(cds)
     gs_encs.append(encs)
 
+####cds = gs_cds[3]
+####encs = gs_encs[3]
+####
+####fig = plt.figure(figsize=(16,9))
+####cresults = sm.OLS(encs,sm.add_constant(cds)).fit()
+####
+####cslope = cresults.params[1]
+####cconstant = cresults.params[0]
+####print cslope, cconstant
+####
+####plt.scatter(cds, encs)
+####plt.plot(cds, np.array(cds)*1.0*cslope+cconstant)
+####plt.show()
+####plt.close()
+#
+gstr = ["25.0mV/fC", "14.0mV/fC", " 7.8mV/fC", " 4.7mV/fC"] 
+fig = plt.figure(figsize=(12,8))
+clor = ['g','b','r','m']
+for i in range(4):
+    cds = gs_cds[i]
+    encs = gs_encs[i]
+    x= np.array([0, 22, 27, 33, 47, 50, 100, 150])
+    yl= [[], [],[], [], [], [], [], []]
+    #x= np.array([0, 27,  50, 100, 150])
+    #yl= [[], [], [], [], []]
+    for j in range(len(cds)):
+        if (cds[j] in x):
+            pos = np.where( np.array(x) == cds[j] )[0][0]
+            yl[pos].append(encs[j])
+    ym = []
+    yr = []
+    for yt in yl:
+        ym.append(np.mean(yt))
+        yr.append(np.std(yt))
+    
+    cresults = sm.OLS(ym,sm.add_constant(x)).fit()
+    
+    cslope = cresults.params[1]
+    cconstant = cresults.params[0]
+    print cslope, cconstant
+    #plt.scatter(cds, encs)
+    label = gstr[i] +  ", Y = (%.2f) * X + (%d)"%(cslope, cconstant)
+    plt.errorbar(x, ym, yr, color=clor[i], fmt = "o")
+    plt.plot(x, np.array(x)*1.0*cslope+cconstant, c=clor[i], label=label)
+    plt.text(x[0], 350+50*i, "%d e$^-$"%(x[0]*1.0*cslope+cconstant),color=clor[i], fontsize = 20 )
+    plt.text(80, 50+50*i, "%d e$^-$"%(80*1.0*cslope+cconstant),color=clor[i], fontsize = 20 )
+    plt.text(120, 50+50*i, "%d e$^-$"%(120*1.0*cslope+cconstant),color=clor[i], fontsize = 20 )
 
-fig = plt.figure(figsize=(16,9))
-plt.scatter(gs_cds[3], gs_encs[3])
-plt.show()
+plt.text(x[0], 350+50*4, "Cd = 0pF", fontsize = 20 )
+plt.text(80, 50+50*4, "Cd = 80pF", fontsize = 20 )
+plt.text(120, 50+50*4, "Cd = 120pF", fontsize = 20 )
+
+plt.title( "ENC vs. Cd @ (tp =3.0us) ", fontsize = 16 )
+plt.ylabel("ENC / e$^-$", fontsize = 16 )
+plt.ylim([0,1000])
+plt.xlabel("Cd / fC", fontsize = 16 )
+plt.xlim([-25, 175])
+plt.legend(loc='best', fontsize = 16)
+plt.tick_params(labelsize=16)
+plt.grid()
+plt.tight_layout( rect=[0.05, 0.05, 0.95, 0.95])
+#plt.show()
+plt.savefig("/Users/shanshangao/Google_Drive_BNL/tmp/caps/" + fn + "_LN_ind_30us_.png",format='png')
 plt.close()
 #
 #ind_chncaps = asic7 + asic8 
---
+++ cap_plot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+                     # apano,   rmsdate,     fpgdate,     asidate, rmsrunno, fpgarunno, asicrunno, apafolder
+python cap_plot.py 9    "10_23_2018" "10_23_2018" "10_23_2018" run04rms   run04fpg  run99asi  SBND 
+
